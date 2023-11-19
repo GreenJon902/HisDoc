450fd3ba85583764bad868e949cdecf9e3a8e387
GreenJon902 <greenjon902@gmail.com>
2023-11-19 11:34:02 +0000

    made css page renderer a thing

---
+++ src/main/java/com/greenjon902/hisdoc/pages/CssPageRenderer.java
@@ -0,0 +1,20 @@
+package com.greenjon902.hisdoc.pages;
+
+import com.greenjon902.hisdoc.webDriver.PageRenderer;
+import com.greenjon902.hisdoc.webDriver.User;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class CssPageRenderer extends PageRenderer {
+	@Override
+	public String render(Map<String, String> query, String fragment, User user) {
+		try {
+			InputStream fileInputStream = this.getClass().getClassLoader().getResourceAsStream("com/greenjon902/hisdoc/pageBuilder/themes/" + query.get("name") + ".css");
+			return new String(fileInputStream.readAllBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- src/test/java/com/greenjon902/hisdoc/UITest.java
+++ src/test/java/com/greenjon902/hisdoc/UITest.java
@@ -14,8 +14,6 @@ import com.greenjon902.hisdoc.webDriver.User;
 import com.greenjon902.hisdoc.webDriver.WebDriver;
 import com.greenjon902.hisdoc.webDriver.WebDriverConfig;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -80,16 +78,6 @@ public class UITest {
 						return pageBuilder.render(user);
 					}
 				}),
-				Map.entry("/" + pageNamePrefix + "themes", new PageRenderer() {
-					@Override
-					public String render(Map<String, String> query, String fragment, User user) {
-						try {
-							InputStream fileInputStream = this.getClass().getClassLoader().getResourceAsStream("com/greenjon902/hisdoc/pageBuilder/themes/" + query.get("name") + ".css");
-							return new String(fileInputStream.readAllBytes());
-						} catch (IOException e) {
-							throw new RuntimeException(e);
-						}
-					}
-				}));
+				Map.entry("/" + pageNamePrefix + "themes", new CssPageRenderer()));
 	}
 }
