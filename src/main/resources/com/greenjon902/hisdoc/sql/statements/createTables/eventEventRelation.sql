a71c37f1a91c0c5bce8c04247d97772731f0a5f2
GreenJon902 <greenjon902@gmail.com>
2024-01-14 14:38:18 +0000

    relation tables dont have their own ids, just composite of where they point

--- src/main/resources/com/greenjon902/hisdoc/sql/statements/createTables/eventEventRelation.sql
+++ src/main/resources/com/greenjon902/hisdoc/sql/statements/createTables/eventEventRelation.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS {prefix}EventEventRelation (
-  eerid INTEGER NOT NULL AUTO_INCREMENT,
   eid1  INTEGER NOT NULL  REFERENCES {prefix}Event(eid),
   eid2  INTEGER NOT NULL  REFERENCES {prefix}Event(eid),
-  UNIQUE (eid1, eid2),
-  PRIMARY KEY(eerid),
+  PRIMARY KEY(eid1, eid2),
   CONSTRAINT CheckEventRelation CHECK (eid1 != eid2)
 );
--- src/main/resources/com/greenjon902/hisdoc/sql/statements/createTables/eventPersonRelation.sql
+++ src/main/resources/com/greenjon902/hisdoc/sql/statements/createTables/eventPersonRelation.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS {prefix}EventPersonRelation (
-  eurid INTEGER NOT NULL AUTO_INCREMENT,
   eid   INTEGER NOT NULL  REFERENCES {prefix}Event(eid),
   pid   INTEGER NOT NULL  REFERENCES {prefix}Person(pid),
-  UNIQUE (eid, pid),
-  PRIMARY KEY(eurid)
+  PRIMARY KEY(eid, pid)
 );
